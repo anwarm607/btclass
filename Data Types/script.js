19750d9f0d7bc88bda40afd9d727609689154269
Anwar <anwar@crayond.co>
2022-12-03 11:33:48 +0530

    Add: Data type code

---
+++ Data Types/script.js
@@ -0,0 +1,27 @@
+//List of data types
+
+let firstName = "Anwar";
+
+let age = 27;
+
+let isMale = true;
+
+let dob;
+
+let address = null;
+
+// Primitive data types
+
+
+console.log(firstName, "String Type");
+console.log(age, "Number Type");
+console.log(isMale, "Boolean Type");
+console.log(dob, "Undefined Type");
+console.log(address, "Null Type");
+
+console.log(typeof firstName, "String Type");
+console.log(typeof age, "Number Type");
+console.log(typeof isMale, "Boolean Type");
+console.log(typeof dob, "Undefined Type");
+console.log(typeof address, "Null Type");
+
