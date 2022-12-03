ebb8148c6247f1a2bb85219013a1b5f6e1652755
Anwar <anwar@crayond.co>
2022-12-03 11:25:04 +0530

    JS Intro class code

---
+++ JS Intro/script.js
@@ -0,0 +1,38 @@
+// Log
+console.log(1224);
+/*Alert id
+asjkdhiuahsdh
+*/
+alert(56 - 3123);
+
+var age = "27";
+console.log(age);
+let firstName = "Anwar";
+firstName = "Mohamed"
+console.log(firstName);
+
+const gender = "Male";
+
+// gender = "Female"
+console.log(gender)
+
+let lastName = "Anwar";
+
+// String concatination
+
+let fullName = firstName + ' ' + lastName;  
+
+
+console.log(fullName);
+
+// Case sensitive
+const Gender = ""
+
+/* Naming Rules:
+    1. No JS keyword
+    2. name should not start with numbers
+    3. No space and hiphens in variable name
+    4. Case sensitive
+    5. Variable name should be meaningful
+    6. Should use camelcase i.e., firstName., first letter of first word will be small and next word should be Capital
+*/
