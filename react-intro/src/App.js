500f27803d5d28743a2da4e02dd99f42314b70f2
Anwar <anwarm607@gmail.com>
2023-01-21 11:48:08 +0530

    React intro

---
+++ react-intro/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+      <h1>Hello</h1>
+      <h2>There</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
